9180aa172060a3e0880db9eb5609b2b8227dba3c
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2024-09-23 21:49:29 +0530

    Refactor Post component to add comment functionality

--- src/pages/Home/Post.tsx
+++ src/pages/Home/Post.tsx
@@ -1,22 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { EditOutlined } from '@ant-design/icons';
-import { Card } from 'antd';
+import { Card, Modal, Form, Input, Divider } from 'antd';
 import { IPost } from '../../models/IPost';
+import axiosInstance from '../../common/axios';
 
 const { Meta } = Card;
 
-const Post: React.FC<IPost> = (post) => (
-  <Card
-    style={{ width: 300 }}
-    actions={[
-      <EditOutlined key="edit" />,
-    ]}
-  >
-    <Meta
-      title={<span style={{ color: post.color }}>{post.title}</span>} // Set title color
-      description={post.description}
-    />
-  </Card>
-);
+const Post: React.FC<IPost> = (post) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [form] = Form.useForm();
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const createComment = async (values: any) => {
+    try {
+      await axiosInstance.post('/post/comment', {...values,postId:post.id});
+      // Optionally, you can trigger a refetch of posts here
+      // fetchPosts();
+      setIsModalVisible(false); // Close the modal
+      form.resetFields(); // Reset the form fields
+    } catch (error) {
+      console.error('Failed to create post:', error);
+    }
+  };
+
+  return (
+    <>
+      <Card
+        style={{ width: 300 }}
+        actions={[
+          <EditOutlined key="edit" onClick={showModal} />,
+        ]}
+      >
+        <Meta
+          title={<span style={{ color: post.color }}>{post.title}</span>}
+          description={post.description}
+        />
+      </Card>
+
+      <Modal
+        title="Edit Post"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Card style={{ width: 300 }}>
+          <Meta
+            title={<span style={{ color: post.color }}>{post.title}</span>}
+            description={post.description}
+          /> 
+        </Card>
+        <Divider />
+        <h3>Comments</h3>
+        {post?.comments.map((comment) => (
+          <Card key={comment.id} style={{ width: 300 }}>
+            <p>{comment.description}</p>
+          </Card>
+        ))}
+        <Divider />
+        <Form form={form} onFinish={createComment}>
+        <Form.Item
+          label="Title"
+          name="title"
+          rules={[{ required: true, message: 'Please input the title!' }]}
+        >
+        <Input.TextArea />
+        </Form.Item>
+      </Form>
+      </Modal>
+    </>
+  );
+};
 
 export default Post;
